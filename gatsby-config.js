b700edc21dde89ef30ee39df5df0608f27519581
Ankit Pal <palankit.ap@gmail.com>
2022-05-30 06:53:39 +0530

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,17 @@
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
+    title: "Hulk Mightest Avenger",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options:{
+        name: `blog`,
+        path: `${__dirname}/blog`
+      }
+    },
   ],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,34 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link,useStaticQuery,graphql } from 'gatsby'
 import {
     container,
     heading,
     navLinks,
     navLinkItem,
-    navLinkText
+    navLinkText,
+    siteTitle,
 
 } from './layout.module.css'
 
 const Layout = ({pageTitle, children}) => {
+    const data = useStaticQuery(graphql`
+    query{
+        site{
+            siteMetadata{
+                title
+            }
+        }
+    }
+    `)
     return(
         <div className={container}>
             <title>{pageTitle}</title>
+            <header className={siteTitle}>{data.site.siteMetadata.title}</header>
             <nav>
                 <ul className={navLinks}>
-                    <li className={navLinkItem}><Link to="/">Home</Link></li>
-                    <li className={navLinkItem}><Link to="/about">About</Link></li>
+                    <li className={navLinkItem}><Link to="/" className={navLinkItem}>Home</Link></li>
+                    <li className={navLinkItem}><Link to="/about" className={navLinkItem}>About</Link></li>
+                    <li className={navLinkItem}><Link to="/blog" className={navLinkItem}>Blog</Link></li>
                 </ul>
             </nav>
             <main>
