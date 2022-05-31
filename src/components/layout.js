874648804683ba04a7e19d3ba80a6b26247ead67
Ankit Pal <palankit.ap@gmail.com>
2022-05-31 12:44:59 +0530

    Finished Gatsby Tutorial Part 6

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,9 +26,9 @@ const Layout = ({pageTitle, children}) => {
             <header className={siteTitle}>{data.site.siteMetadata.title}</header>
             <nav>
                 <ul className={navLinks}>
-                    <li className={navLinkItem}><Link to="/" className={navLinkItem}>Home</Link></li>
-                    <li className={navLinkItem}><Link to="/about" className={navLinkItem}>About</Link></li>
-                    <li className={navLinkItem}><Link to="/blog" className={navLinkItem}>Blog</Link></li>
+                    <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
+                    <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
+                    <li className={navLinkItem}><Link to="/blog" className={navLinkText}>Blog</Link></li>
                 </ul>
             </nav>
             <main>
